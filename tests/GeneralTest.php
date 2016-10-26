c4b1bcaf10bb0f12645fe4bf2e025ddb667076b0
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-10-26 12:23:31 -0400

    testing that travis hook

--- tests/GeneralTest.php
+++ tests/GeneralTest.php
@@ -4,6 +4,6 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
 {
     public function testMe()
     {
-        $this->assertTrue(1 + 1 === 1);
+        $this->assertTrue(1 + 1 === 2);
     }
 }
